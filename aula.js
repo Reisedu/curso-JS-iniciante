c6e8ce2354dcd2acf4974d32572909982bdb4531
eduardo <reiseduardo2020@gmail.com>
2022-09-25 23:02:50 -0300

    estudo JS

---
+++ aula.js
@@ -0,0 +1,29 @@
+// mostrar no console
+    console.log('eduardo')
+    console.log('ta dando bom')
+
+// declarar variavel
+    let idade = 25;
+    console.log(idade);
+    let altura = 1.54;
+    console.log(altura);
+        // para string (letras) colocar em aspas
+    let amor = 'larissa;'
+    console.log(amor);
+    let dataCasamento = '07 de janeiro de 2019'
+    console.log(dataCasamento)
+
+    // alterar valor na variavel (utilizar LET)
+    let valorIngressoAdulto = 20;
+    valorIngressoAdulto = 30;
+    console.log(valorIngressoAdulto);
+
+// constante 
+    const idadeLarissa = 26;
+    console.log(idadeLarissa);
+
+
+
+
+
+
